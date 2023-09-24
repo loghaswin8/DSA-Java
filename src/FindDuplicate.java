f03fdc7df82e8a7b7a39e2240567ff9cc6b19e28
Aswin <loghaswinraja08@gmail.com>
2023-09-24 12:11:14 +0530

    commits made to find duplicate numbers in an array.

---
+++ src/FindDuplicate.java
@@ -0,0 +1,26 @@
+class FindDuplicate {
+    public int findDuplicate(int[] nums) {
+        int i = 0;
+
+            while (i < nums.length) {
+                if (nums[i] != i + 1) {
+                    int correct = nums[i];
+                    if (nums[i] < nums.length && nums[i] != nums[correct]) {
+                        swap(nums, i, correct);
+                    } else {
+                        return nums[i];
+                    }
+                } else {
+                    i++;
+                }
+        }
+       return -1;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+
+    }
+}
