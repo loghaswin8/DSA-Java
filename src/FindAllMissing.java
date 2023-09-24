5b50be7f0bed17c1d27f83c2f2e2a3164c6cebfa
Aswin <loghaswinraja08@gmail.com>
2023-09-24 12:10:10 +0530

    commits made to find all missing number in an array.

---
+++ src/FindAllMissing.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class FindAllMissing {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int i = 0;
+        while (i< nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else {
+                i++;
+            }
+        }
+
+        //find all missing numbers
+        List<Integer> ans = new ArrayList<>();
+        for(int index = 0; index < nums.length; index++) {
+            if (nums[index] != index + 1) {
+                ans.add(index + 1);
+            }
+        }
+
+        return ans;
+    }
+
+    static void sort(int[] arr) {
+
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+
+    }
+}
